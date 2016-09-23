cd023a24f414e908dd5c3591d80fc46f22953335
Sean <shironlai@sina.com>
2016-09-23 11:54:39 +0800

    add enemy

--- planeFight.py
+++ planeFight.py
@@ -3,11 +3,13 @@
 background_image_filename = 'img\\background.png'
 mouse_image_filename = 'img\\hero.png'
 bullet_image_filename = 'img\\bullet.png'
+enemy_image_filename = 'img\\enemy2.png'
 #指定图像文件名称
 SCREEN_SIZE = (380,680)
  
 import pygame #导入pygame库
 from sys import exit #向sys模块借一个exit函数用来退出程序
+from random import randint
 import os
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (500,20) # 控制窗口打开位置
 
@@ -27,6 +29,20 @@ class Bullet(object):
         else:
             self.y -= self.speed*passed_time_seconds
 
+class Enemy(object):#定义一个Enemy类，封装敌机的数据和方法
+    def restart(self):
+        self.x = randint(-30,400)
+        self.y = randint(-100, -50)
+        self.speed = randint(100,400)
+    def __init__(self):
+        self.restart()
+        self.image = pygame.image.load(enemy_image_filename).convert_alpha()
+
+    def move(self, passed_time_seconds):
+        if self.y < 650:
+            self.y += self.speed*passed_time_seconds
+        else:
+            self.restart()
 
 pygame.init() #初始化pygame,为使用硬件做准备
 screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
@@ -39,6 +55,7 @@ mouse_cursor = pygame.image.load(mouse_image_filename).convert_alpha()
 
 clock = pygame.time.Clock()
 bullet = Bullet()
+enemy = Enemy()
 
 while True:
 #游戏主循环
@@ -48,18 +65,19 @@ while True:
             #接收到退出事件后退出程序
             pygame.quit()
             exit()
-
-    screen.blit(background, (0,0))
-    #将背景图画上去
     time_passed = clock.tick(100)
     time_passed_seconds = time_passed/1000.0
+    screen.blit(background, (0,0))
+    #将背景图画上去
     x, y = pygame.mouse.get_pos()
     #获得鼠标位置
     bullet.move(time_passed_seconds)
+    enemy.move(time_passed_seconds)
 
     x-= mouse_cursor.get_width() / 2
     y-= mouse_cursor.get_height() / 2
     #计算光标的左上角位置
+    screen.blit(enemy.image, (enemy.x, enemy.y))
     screen.blit(bullet.image, (bullet.x, bullet.y))
     screen.blit(mouse_cursor, (x, y))
     #把光标画上去
