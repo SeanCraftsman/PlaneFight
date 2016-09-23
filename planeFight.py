00bd6132337c0087a8765a4089c49e6e8ef4cc9d
Sean <shironlai@sina.com>
2016-09-23 11:02:02 +0800

    add bullet

--- planeFight.py
+++ planeFight.py
@@ -2,6 +2,7 @@
 
 background_image_filename = 'img\\background.png'
 mouse_image_filename = 'img\\hero.png'
+bullet_image_filename = 'img\\bullet.png'
 #指定图像文件名称
  
 import pygame #导入pygame库
@@ -15,12 +16,14 @@ screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
 #创建了一个窗口
 pygame.display.set_caption("PlaneFight!")
 #设置窗口标题
+pygame.mouse.set_visible(False) # 隐藏鼠标指针
  
 background = pygame.image.load(background_image_filename).convert()
 mouse_cursor = pygame.image.load(mouse_image_filename).convert_alpha()
+bullet = pygame.image.load(bullet_image_filename).convert_alpha()
 #加载并转换图像
 
-pygame.mouse.set_visible(False) # 隐藏鼠标指针
+bullet_x, bullet_y = 0, -100
 
 while True:
 #游戏主循环
@@ -33,14 +36,19 @@ while True:
  
     screen.blit(background, (0,0))
     #将背景图画上去
- 
     x, y = pygame.mouse.get_pos()
     #获得鼠标位置
+    if bullet_y < -1 :
+    	bullet_x, bullet_y = x,y
+    else:
+    	bullet_y -= 1
     x-= mouse_cursor.get_width() / 2
     y-= mouse_cursor.get_height() / 2
     #计算光标的左上角位置
+    screen.blit(bullet, (bullet_x, bullet_y))
     screen.blit(mouse_cursor, (x, y))
     #把光标画上去
+    
  
     pygame.display.update()
     #刷新一下画面
