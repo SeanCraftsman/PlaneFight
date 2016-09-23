6ec230e0dc55630d1ca7149a2f5be8b902661969
Sean <shironlai@sina.com>
2016-09-23 11:48:21 +0800

    add bullet class

--- planeFight.py
+++ planeFight.py
@@ -4,28 +4,42 @@ background_image_filename = 'img\\background.png'
 mouse_image_filename = 'img\\hero.png'
 bullet_image_filename = 'img\\bullet.png'
 #指定图像文件名称
+SCREEN_SIZE = (380,680)
  
 import pygame #导入pygame库
 from sys import exit #向sys模块借一个exit函数用来退出程序
 import os
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (500,20) # 控制窗口打开位置
 
+#定义一个Bullet类，封装子弹的数据和方法
+class Bullet(object):
+    def __init__(self):
+        self.x = 0
+        self.y = -100
+        self.speed = 600
+        self.image = pygame.image.load(bullet_image_filename).convert_alpha()
+
+    def move(self,passed_time_seconds):
+        if self.y < 0:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            self.x = mouseX - self.image.get_width()/2
+            self.y = mouseY - self.image.get_width()/2
+        else:
+            self.y -= self.speed*passed_time_seconds
+
+
 pygame.init() #初始化pygame,为使用硬件做准备
-SCREEN_SIZE = (380,680)
 screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
-#创建了一个窗口
 pygame.display.set_caption("PlaneFight!")
-#设置窗口标题
 pygame.mouse.set_visible(False) # 隐藏鼠标指针
- 
+
 background = pygame.image.load(background_image_filename).convert()
 mouse_cursor = pygame.image.load(mouse_image_filename).convert_alpha()
-bullet = pygame.image.load(bullet_image_filename).convert_alpha()
 #加载并转换图像
 
-bullet_x, bullet_y = 0, -100
-bullet_speed = 500
 clock = pygame.time.Clock()
+bullet = Bullet()
+
 while True:
 #游戏主循环
  
@@ -41,15 +55,12 @@ while True:
     time_passed_seconds = time_passed/1000.0
     x, y = pygame.mouse.get_pos()
     #获得鼠标位置
-    if bullet_y < -1 :
-        bullet_x, bullet_y = x,y
-    else:
-        bullet_y -= bullet_speed * time_passed_seconds
-        #print time_passed_seconds
+    bullet.move(time_passed_seconds)
+
     x-= mouse_cursor.get_width() / 2
     y-= mouse_cursor.get_height() / 2
     #计算光标的左上角位置
-    screen.blit(bullet, (bullet_x, bullet_y))
+    screen.blit(bullet.image, (bullet.x, bullet.y))
     screen.blit(mouse_cursor, (x, y))
     #把光标画上去
  
